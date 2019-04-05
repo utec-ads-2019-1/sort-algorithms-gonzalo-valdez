d533d6ad2b2a66efe8c3a7832affa90258c0674b
gvc99 <gonzaabc@hotmail.com>
2019-04-04 19:38:37 -0500

    update

--- main.cpp
+++ main.cpp
@@ -19,7 +19,6 @@ Mocker mocker;
 
 void executeTests();
 void printArray(int *, size_t);
-void compareIntegers(void *, void *);
 
 int main(int argc, char* argv[]) {
     cout << "===========================================================" << endl;
@@ -31,18 +30,11 @@ int main(int argc, char* argv[]) {
     return EXIT_SUCCESS;
 }
 
-void compareIntegers(void *elements, int first, int second) {
-    int *array = (int*) elements;
-    if (array[first] > array[second]) {
-        swap(array[first], array[second]);
-    }
-}
-
 void executeTests() {
     for (int i = 0; i < NUMBER_OF_TESTS; ++i) {
         size_t size = mocker.generateRandomInt();
         int *array = mocker.generateRandomIntArray(size);
-        Tester::integerSorts(array, size, compareIntegers);
+        Tester::integerSorts(array, size);
         cout << "Passed test: " << i + 1 << endl;
     }
 }
